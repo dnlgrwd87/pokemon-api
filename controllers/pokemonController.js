65cfbe4d0ed1f2ae3bdacbfcccc3edca0bd702b6
dnlgrwd87 <dgarwood@uncc.edu>
2020-04-07 19:48:46 -0400

    Add query param to get trainer with pokemon

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -11,17 +11,19 @@ module.exports.getAllPokemon = async (req, res) => {
         const response = await getAllPokemon();
         return res.status(200).json(response.rows);
     } catch (error) {
-        res.status(400).json(error);
+        res.json(error);
     }
 };
 
 module.exports.getPokemonById = async (req, res) => {
     try {
         const response = await getPokemonById(req.params.id);
-        if (response.rows && !response.rows.length) throw new Error('Not found');
+        if (response.rows && !response.rows.length) {
+            throw new Error('pokemon not found');
+        }
         return res.status(200).json(response.rows[0]);
     } catch (error) {
-        res.status(404).json({ error: error.message });
+        res.json({ error: error.message });
     }
 };
 
--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -1,10 +1,22 @@
 const { getTrainerById } = require('../services/trainerService');
+const { getAllPokemonByTrainerId } = require('../services/pokemonService');
 
 module.exports.getTrainerById = async (req, res) => {
     try {
-        const response = await getTrainerById(req.params.id);
-        res.status(200).json(response.rows[0]);
+        const trainerResponse = await getTrainerById(req.params.id);
+        if (trainerResponse.rows && !trainerResponse.rows.length) {
+            throw new Error('trainer not found');
+        }
+        const trainer = trainerResponse.rows[0];
+        if (req.query.pokemon) {
+            const pokemonResponse = await getAllPokemonByTrainerId(req.params.id);
+            trainer.pokemon = pokemonResponse.rows.map(pkmn => {
+                const { id, name, pokedex_id, image_url } = pkmn;
+                return { id, name, pokedex_id, image_url };
+            });
+        }
+        res.status(200).json(trainer);
     } catch (error) {
-        res.json(error);
+        res.json({ error: error.message });
     }
 };
